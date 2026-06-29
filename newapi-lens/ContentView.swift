dd82f22f3c40f66074fbc93a7fd0f37292b35534
Linying Assad <linying@apache.org>
2026-06-29 13:58:31 +0800

    Initial Commit

---
+++ newapi-lens/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  newapi-lens
+//
+//  Created by Linying Assad on 2026/6/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ newapi-lens/newapi_lensApp.swift
@@ -0,0 +1,17 @@
+//
+//  newapi_lensApp.swift
+//  newapi-lens
+//
+//  Created by Linying Assad on 2026/6/29.
+//
+
+import SwiftUI
+
+@main
+struct newapi_lensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
